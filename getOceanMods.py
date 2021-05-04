0677a51a603a0bfc07bcd95769dce94f7fd75649
Paul J. Durack <durack1@llnl.gov>
2021-05-04 14:32:46 -0700

    Comment, redundant import cleanup

--- getOceanMods.py
+++ getOceanMods.py
@@ -9,9 +9,7 @@ Created on Tue May  4 14:27:40 2021
 import datetime
 import json
 import os
-#import pdb
 import shutil
-#import sys
 from esgfQueryModels import get_dataset_time_data
 
 #%% Get time
@@ -50,11 +48,3 @@ for mipEra in mips.keys():
         print('JSON response numFound:', js['response']['numFound'])
         with open(outFile, 'w', encoding='utf-8') as f:
             json.dump(js, f, ensure_ascii=False, indent=4)
-
-#%%
-#3982
-#https://esgf-node.llnl.gov/solr/datasets/select?q=*:*&wt=json&facet=true&rows=4000&fq=type:Dataset&fq=mip_era:CMIP6&fq=activity_id:CMIP&fq=experiment_id:historical&fq=variable_id:tos&shards=localhost:8983/solr/datasets,localhost:8985/solr/datasets,localhost:8987/solr/datasets,localhost:8988/solr/datasets,localhost:8990/solr/datasets,localhost:8993/solr/datasets,localhost:8994/solr/datasets,localhost:8995/solr/datasets,localhost:8996/solr/datasets,localhost:8997/solr/datasets
-#2204
-#https://esgf-node.llnl.gov/solr/datasets/select?q=*:*&wt=json&facet=true&rows=2161&fq=type:Dataset&fq=mip_era:CMIP6&fq=activity_id:CMIP&fq=experiment_id:historical&fq=variable_id:tos
-#Shards
-#https://esgf-node.llnl.gov/esg-search/search/?limit=0&format=application%2Fsolr%2Bjson
